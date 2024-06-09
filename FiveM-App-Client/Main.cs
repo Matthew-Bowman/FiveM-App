02e4ecbf6b9cb080a518b62430ebe1212e900530
Matthew <Matthew@MATTHEW_DESKTOP>
2024-06-09 20:12:15 +0100

    Add project files.

---
+++ FiveM-App-Client/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace FiveM_App_Client
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+
+        }
+    }
+}
---
+++ FiveM-App/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace FiveM_App_Server
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+        }
+    }
+}
