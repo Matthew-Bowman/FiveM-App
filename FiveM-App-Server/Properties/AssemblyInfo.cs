c91dcadf949177ee9b9d4f6aca693009fe68a24e
Matthew <matthew.bowman@tutanota.com>
2024-06-12 00:58:08 +0100

    Renamed FiveM-App to FiveM-App-Server

--- FiveM-App-Server/Properties/AssemblyInfo.cs
+++ FiveM-App-Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FiveM-App")]
+[assembly: AssemblyTitle("FiveM-App-Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FiveM-App")]
+[assembly: AssemblyProduct("FiveM-App-Server")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
