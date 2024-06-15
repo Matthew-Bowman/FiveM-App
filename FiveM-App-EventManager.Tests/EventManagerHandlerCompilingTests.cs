a4da314e14d66c521d4dec8d8a934c0217ee0c13
Matthew <matthew.bowman@tutanota.com>
2024-06-15 01:34:02 +0100

    Created Handler Compiling Tests Class

---
+++ FiveM-App-EventManager.Tests/EventManagerHandlerCompilingTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FiveM_EventManager;
+using Xunit;
+
+namespace FiveM_App_EventManager_Tests
+{
+    public class EventManagerHandlerCompilingTests
+    {
+    }
+}
