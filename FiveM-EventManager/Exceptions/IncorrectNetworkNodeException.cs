7f9a0e160e7e10c653741918b24830c7bcef4306
Matthew <matthew.bowman@tutanota.com>
2024-06-15 02:00:27 +0100

    Made Exception Public

--- FiveM-EventManager/Exceptions/IncorrectNetworkNodeException.cs
+++ FiveM-EventManager/Exceptions/IncorrectNetworkNodeException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FiveM_EventManager.Exceptions
 {
-    internal class IncorrectNetworkNodeException : Exception
+    public class IncorrectNetworkNodeException : Exception
     {
     }
 }
