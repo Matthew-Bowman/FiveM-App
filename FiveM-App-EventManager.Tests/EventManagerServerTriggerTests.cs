8686505c7cf06fdafae5988b7a968b31b2a77ec4
Matthew <matthew.bowman@tutanota.com>
2024-06-17 20:50:42 +0100

    Added Test Cases For Server Event Triggers

---
+++ FiveM-App-EventManager.Tests/EventManagerServerTriggerTests.cs
@@ -0,0 +1,49 @@
+﻿using FiveM_EventManager;
+using FiveM_EventManager.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveM_App_EventManager_Tests
+{
+    public class EventManagerServerTriggerTests
+    {
+        /*************** TESTS ***************/
+        [Fact]
+        public void TriggerServerEventTest()
+        {
+            // Arrange
+            ITriggerEvents eventManager = new EventManager(NetworkNode.Client);
+
+            ServerEvent serverEvent = ServerEvent.XUnitTestEvent;
+            string eventData = "{}";
+
+            // Act
+            Exception exception = Record.Exception(() => eventManager.TriggerEvent(serverEvent, eventData));
+
+            // Assert
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void InvalidTriggerEventTest()
+        {
+            // Arrange
+            ITriggerEvents eventManager = new EventManager(NetworkNode.Client);
+
+            ClientEvent clientEvent = ClientEvent.XUnitTestEvent;
+            string eventData = "{}";
+
+            // Act & Assert
+            Assert.Throws<IncorrectNetworkNodeException>(() => eventManager.TriggerEvent(clientEvent, eventData));
+        }
+
+
+
+
+
+        /*************** HELPER FUNCTIONS ***************/
+    }
+}
