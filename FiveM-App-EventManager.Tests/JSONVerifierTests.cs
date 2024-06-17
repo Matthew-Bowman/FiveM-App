0f387deda52abd2ca83b4f3fddeb220f2bb55b97
Matthew <matthew.bowman@tutanota.com>
2024-06-17 21:15:08 +0100

    Added Test Cases For Verifying JSON

---
+++ FiveM-App-EventManager.Tests/JSONVerifierTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FiveM_EventManager;
+
+namespace FiveM_App_EventManager_Tests
+{
+    public class JSONVerifierTests
+    {
+        /*************** TESTS ***************/
+        [Fact]
+        public void VerifySimpleJSON()
+        {
+            // Arrange
+            IVerifyJSON jsonVerifier = new JSONVerifier();
+
+            string jsonData = "{\"key\":\"value\"}";
+
+            // Act
+            bool validJSON = jsonVerifier.VerifyJSON(jsonData);
+
+            // Assert
+            Assert.True(validJSON);
+        }
+    }
+}
