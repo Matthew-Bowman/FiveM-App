747dc6dba09c05eecf47955ab7170ce3e246fd6b
Matthew <matthew.bowman@tutanota.com>
2024-06-17 20:40:07 +0100

    Wrote Test Cases For Client Handler Compiling

---
+++ FiveM-App-EventManager.Tests/EventManagerClientHandlerCompilingTests.cs
@@ -0,0 +1,58 @@
+﻿using FiveM_EventManager.Exceptions;
+using FiveM_EventManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveM_App_EventManager_Tests
+{
+    public class EventManagerClientHandlerCompilingTests
+    {
+        /*************** TESTS ***************/
+
+        [Fact]
+        public void CompileHandlersTest()
+        {
+            // Arrange
+            IHandleEvents eventHandler = new EventManager(NetworkNode.Client);
+
+            ClientEvent clientEvent = ClientEvent.XUnitTestEvent;
+            Action<string> eventAction = new Action<string>(OnTestEvent);
+
+            eventHandler.AddPseudoHandler(clientEvent, eventAction);
+
+            // Act
+            Exception exception = Record.Exception(() => eventHandler.CompileEvents());
+
+            // Assert
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void AlreadyCompiledTest()
+        {
+            // Arrange
+            IHandleEvents eventHandler = new EventManager(NetworkNode.Server);
+
+            ClientEvent clientEvent = ClientEvent.XUnitTestEvent;
+            Action<string> eventAction = new Action<string>(OnTestEvent);
+
+            eventHandler.AddPseudoHandler(clientEvent, eventAction);
+
+            // Act
+            eventHandler.CompileEvents();
+
+            // Assert
+            Assert.Throws<HandlersAlreadyCompiledException>(() => eventHandler.CompileEvents());
+        }
+
+
+
+
+
+        /*************** HELPER FUNCTIONS ***************/
+        private void OnTestEvent(string pData) { }
+    }
+}
